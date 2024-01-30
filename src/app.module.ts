676ce41810a37f4efab63f028eba4b8edc687b44
Renato Santos <renato.santos0611@gmail.com>
2024-01-30 14:27:03 -0300

    make doent work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { WithPaginateModule } from './with-paginate/with-paginate.module';
 import { WithoutPaginateModule } from './without-paginate/without-paginate.module';
+import { WithPaginateModule } from './with-paginate/with-paginate.module';
 
 @Module({
   imports: [WithoutPaginateModule, WithPaginateModule],
